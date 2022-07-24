6ac7e4e29a27e26a1b939025a28679f83f754920
İnciKüçük <56034341+incikucuk@users.noreply.github.com>
2022-07-24 15:58:27 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import ProductList from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
+import React, { Component } from "react";
+
+export default class App extends Component {
+  state = { currentCategory: "", products: [] };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  changeCategory = category => {
+    this.setState({ currentCategory: category.categoryName });
+    this.getProducts(category.id);
+    
+  };
+
+  getProducts = categoryId => {
+    let url="http://localhost:3000/products";
+    if(categoryId){
+      url +="?categoryId="+categoryId;
+    }
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => this.setState({ products: data }));
+  };
+  render() {
+    let productInfo = { title: "ProductList" };
+    let categoryInfo = { title: "CategoryList" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              />
+            </Col>
+            <Col xs="9">
+              <ProductList
+                products={this.state.products}
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={productInfo}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
