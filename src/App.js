305610c0f42adb87170df3abf36c4320dab50e61
İnciKüçük <56034341+incikucuk@users.noreply.github.com>
2022-07-26 23:04:12 +0300

    AddToCart-1.2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navi from "./Navi";
 import ProductList from "./ProductList";
 import { Container, Row, Col } from "reactstrap";
 import React, { Component } from "react";
+import alertify from "alertifyjs"
 
 export default class App extends Component {
   state = { currentCategory: "", products: [],cart:[] };
@@ -35,7 +36,16 @@ export default class App extends Component {
 
       }
       this.setState({cart:newCart});
+      alertify.success(product.productName+" added to cart!",2);
     }
+
+    removeFromCart=(product)=>{
+      let newCart = this.state.cart.filter(c=>c.product.id!==product.id);
+      this.setState({cart:newCart})
+    
+    }
+
+
     
   render() {
     let productInfo = { title: "ProductList" };
@@ -43,7 +53,7 @@ export default class App extends Component {
     return (
       <div>
         <Container>
-          <Navi cart={this.state.cart}/>
+          <Navi removeFromCart={this.removeFromCart} cart={this.state.cart}/>
           <Row>
             <Col xs="3">
               <CategoryList
--- src/Navi.js
+++ src/Navi.js
@@ -1,4 +1,5 @@
-import React, { createRef, useState } from 'react';
+/* eslint-disable react/jsx-no-undef */
+import React from "react";
 import {
   Collapse,
   Navbar,
@@ -8,26 +9,22 @@ import {
   NavItem,
   NavLink,
   NavbarText,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-} from 'reactstrap';
-import CartSummary from './CartSummary';
+} from "reactstrap";
 
+import CartSummary from './CartSummary.js';
 
 export default class Navi extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.toogle = this.toogle.bind(this);
-    this.state={
-      isOpen:false
+    this.state = {
+      isOpen: false,
     };
   }
-  toogle(){
+  toogle() {
     this.setState({
-      isOpen:this.state.isOpen
+      isOpen: this.state.isOpen,
     });
   }
 
@@ -35,102 +32,25 @@ export default class Navi extends React.Component {
     return (
       <div>
         <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">Northwind App</NavbarBrand>
-        <NavbarToggler onClick={this.toggle} />
-        <Collapse isOpen={this.isOpen} navbar>
-          <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options - {this.props.cart.length}
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
-        </Collapse>
-       </Navbar>
+          <NavbarBrand href="/">Northwind App</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.isOpen} navbar>
+            <Nav className="me-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              
+              <CartSummary removeFromCart={this.props.removeFromCart} cart={this.props.cart} />
+            </Nav>
+            <NavbarText>Northwind</NavbarText>
+          </Collapse>
+        </Navbar>
       </div>
     );
   }
 }
-
-/*
-
-import React, { useState } from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
-} from 'reactstrap';
-
-const Example = (args) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
-
-  return (
-    <div>
-      <Navbar {...args}>
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
-        </Collapse>
-      </Navbar>
-    </div>
-  );
-}
-
-export default Example;
-*/
